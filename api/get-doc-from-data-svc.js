5a34e3002e082f87eec23bdb77647ba7363d38b4
tripott <tripott@gmail.com>
2022-03-23 18:55:09 +0000

    lint, prettier

--- api/get-doc-from-data-svc.js
+++ api/get-doc-from-data-svc.js
@@ -1,18 +1,3 @@
-import { connect } from 'hyper-connect'
-
-if (!process.env.HYPER) {
-  console.log('****************')
-  console.log(
-    "ERROR:  MISSING HYPER CONNECTION STRING.  See Getting Started: https://docs.hyper.io/getting-started ",
-  )
-  console.log('****************')
-}
-
-const hyper = connect(process.env.HYPER)
-
-
 export default async function (req, res) {
-    console.log('getting doc: ', req.params.id)
-   
-    return res.send("Getting a doc is Incomplete!")
-  }
+  return res.send("Getting a doc is Incomplete!");
+}
--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
 
-import addDoc from './api/add-doc-to-data-svc.js'
-import getDoc from './api/get-doc-from-data-svc.js'
+import addDoc from "./api/add-doc-to-data-svc.js";
+import getDoc from "./api/get-doc-from-data-svc.js";
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.post('/docs', addDoc)
-app.get('/docs/:id', getDoc)
+app.post("/docs", addDoc);
+app.get("/docs/:id", getDoc);
 
+app.get("/", function (req, res) {
+  res.send({ name: "Getting Started Guide Express API", ok: true });
+});
 
-app.get('/', function (req, res) {
-  res.send({ name: 'Getting Started Guide Express API', ok: true })
-})
-
-app.listen(3001)
+app.listen(3001);
